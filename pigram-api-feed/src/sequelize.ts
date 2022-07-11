98a84b6db80ad06d7bccf5f288d132dca612512b
Ahmed <mdbello2495@gmail.com>
2022-07-11 04:30:11 +0100

    Updated Connection

--- pigram-api-feed/src/sequelize.ts
+++ pigram-api-feed/src/sequelize.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import { Dialect } from 'sequelize/types';
 import { config } from './config/config';
 
-export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:5432/${config.database}`)
+export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}/${config.database}`)
 // export const sequelize = new Sequelize(
 //   config.database,
 //   config.username,
