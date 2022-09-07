416bf1ba2f0098d257bb6fd9f6c2e97647499069
Ahmed <52006154+ahmedb24@users.noreply.github.com>
2022-09-07 17:24:38 +0100

    Refactor aws.ts
    refactor: Update typo

--- pigram-api-feed/src/aws.ts
+++ pigram-api-feed/src/aws.ts
@@ -15,7 +15,7 @@ export const s3 = new AWS.S3({
   params: {Bucket: AWS_MEDIA_BUCKET},
 });
 
-// Generates an AWS signed URL for retrieving objects
+// Generates an AWS Signed URL for retrieving objects
 export function getGetSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
 
